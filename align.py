ebce861fb4165af14026a96e5f9c9d03b4e0a12e
Ceres <guyihao0201@gmail.com>
2023-06-08 21:38:23 +0800

    图像拼接

--- align.py
+++ align.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-global MAX_FEATURES
 MAX_FEATURES = 500
+corners = None
 
-def align(refImg, alignImg):
+def Align(refImg, alignImg):
 
     # 将图像上传到GPU
     gpuRefImg = cv2.cuda_GpuMat()
@@ -41,18 +41,113 @@ def align(refImg, alignImg):
     alignPts = np.float32([alignKp[m.trainIdx].pt for m in goodMatches])
     M, mask = cv2.findHomography(alignPts, refPts, cv2.RANSAC)
     
-    # 对齐
-    alignedImg = cv2.warpPerspective(alignImg, M, (refImg.shape[1], refImg.shape[0]))
+    # 进行透视变换
+    CalcCorners(alignImg, M)
+    alignedImg = cv2.warpPerspective(alignImg, M, (max(corners[2][0][0],corners[3][0][0]), refImg.shape[0]))
+    
     return matchedImg, alignedImg
+
+# 计算透视变换后的四个角坐标
+def CalcCorners(img, H):
+    h,w = img.shape[:2]
+    pts = np.float32([[0,0],[0,h],[w,h],[w,0]]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,H)
+    global corners
+    corners = np.int32(dst)
+
+# 优化连接处的拼接
+def OptimizeSeam(refImg, alignedImg, stitchedImg):
+    st = min(corners[0][0][0], corners[1][0][0])
+    width = refImg.shape[1] - st
+    rows = stitchedImg.shape[0]
+    cols = refImg.shape[1]
+    for y in range(rows):
+        for x in range(st, cols):
+            if alignedImg[y, x, 0] == 0 and alignedImg[y, x, 1] == 0 and alignedImg[y, x, 2] == 0:
+                alpha = 1
+            else:
+                alpha = (width - x + st) / width
+            stitchedImg[y, x, 0] = alpha * refImg[y, x, 0] + (1 - alpha) * alignedImg[y, x, 0]
+            stitchedImg[y, x, 1] = alpha * refImg[y, x, 1] + (1 - alpha) * alignedImg[y, x, 1]
+            stitchedImg[y, x, 2] = alpha * refImg[y, x, 2] + (1 - alpha) * alignedImg[y, x, 2]
+    return stitchedImg
+            
+
+# 自我实现的拼接函数
+def MyStitch(refImg, alignedImg, alignImg):
+    dst_width = alignedImg.shape[1]
+    dst_height = refImg.shape[0]
+    dst = np.zeros((dst_height, dst_width, 3), dtype=np.uint8)
+    dst[0:dst_height, 0:dst_width] = alignedImg
+    dst[0:dst_height, 0:refImg.shape[1]] = refImg
+    ShowImg(dst)
+    dst1 = OptimizeSeam(refImg, alignedImg, dst)
+    return dst, dst1
+
+# 拼接配准后的图像和基准图像
+def Stitch(refImg, alignImg):
+    
+    # 创建拼接器
+    stitcher = cv2.Stitcher.create(cv2.Stitcher_PANORAMA)
+    # 拼接
+    status, pano = stitcher.stitch([refImg, alignImg])
+    # 黑边处理
+    if status == cv2.Stitcher_OK:
+        # 全景图轮廓提取
+        stitched = cv2.copyMakeBorder(pano, 10, 10, 10, 10, cv2.BORDER_CONSTANT, (0, 0, 0))
+        gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
+        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+        cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+ 
+        # 轮廓最小正矩形
+        mask = np.zeros(thresh.shape, dtype="uint8")
+        (x, y, w, h) = cv2.boundingRect(cnts[0])  # 取出list中的轮廓二值图，类型为numpy.ndarray
+        cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
+ 
+        # 腐蚀处理，直到minRect的像素值都为0
+        minRect = mask.copy()
+        sub = mask.copy()
+        while cv2.countNonZero(sub) > 0:
+            minRect = cv2.erode(minRect, None)
+            sub = cv2.subtract(minRect, thresh)
+ 
+        # 提取minRect轮廓并裁剪
+        cnts = cv2.findContours(minRect, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        (x, y, w, h) = cv2.boundingRect(cnts[0])
+        stitched = stitched[y:y + h, x:x + w]
+ 
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        # ShowImg(stitched)
+    else:
+        print('图像匹配的特征点不足')
+    return pano, stitched
+
+def ShowResizedImg(img, rate, imgName='res'):
+    cv2.imshow('res', cv2.resize(img,None,fx=rate,fy=rate,interpolation=cv2.INTER_CUBIC))
+    cv2.waitKey(0)
+
+def ShowImg(img, imgName='res'):
+    cv2.imshow('res', img)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
     # load
-    refImg = cv2.imread('./ref.jpg')
-    alignImg = cv2.imread('./align.jpg')
+    refImg = cv2.imread('img/2.jpg')
+    alignImg = cv2.imread('img/1.jpg')
 
     # # 显示原始图像
     # cv2.imshow('refImg', refImg)
     # cv2.imshow('alignImg', alignImg)
-    # cv2.waitKey(0)
+    cv2.waitKey(0)
 
-    matced_img, alignedImg = align(refImg, alignImg)
+    matchedImg, alignedImg = Align(refImg, alignImg)
+    # ShowResizedImg(matchedImg, 0.5)
+    # cv2.imshow('refImg', refImg)
+    # ShowImg(alignedImg)
+    stitchedImg, stitchedImg1 = MyStitch(refImg, alignedImg, alignImg)
+    cv2.imwrite('img/stitchedImg.jpg', stitchedImg)
+    cv2.imwrite('img/stitchedImg1.jpg', stitchedImg1)
+    # stitchedImg = Stitch(refImg, alignImg)
+    # cv2.imshow("拼接图像", stitchedImg)
+    # cv2.waitKey(0)
--- test_pyqt.py
+++ test_pyqt.py
@@ -1,10 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QFileDialog,QGraphicsScene
-from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QFileDialog,QGraphicsScene,QMessageBox
+from PyQt5.QtGui import QPixmap,QImage
+from PyQt5.QtCore import Qt
 from Ui_test import Ui_MainWindow   #导入你写的界面类
 from Ui_alignDialog import Ui_Dialog as alignDialog  #导入窗体类
- 
- 
+from align import Align
+import cv2
+
 class MyMainWindow(QMainWindow,Ui_MainWindow): #这里也要记得改
     def __init__(self,parent =None):
         super(MyMainWindow,self).__init__(parent)
@@ -14,21 +16,45 @@ class AlignDialog(QWidget,alignDialog):
     def __init__(self,parent =None):
         super(AlignDialog,self).__init__(parent)
         self.setupUi(self)
+        self.buttonUploadRefImg.clicked.connect(lambda:OpenImage(self, self.refImgView, 'ref'))
+        self.buttonUploadAlignImg.clicked.connect(lambda:OpenImage(self, self.alignImgView, 'align'))
+        self.buttonAlign.clicked.connect(lambda:AlignImage(self, self.alignedImgView))
+        self.img = {}
         
-def openImage(self, imgView):  # 选择本地图片上传
+def OpenImage(self, imgView, imgG):  # 选择本地图片上传
     imgName, imgType = QFileDialog.getOpenFileName(self, "上传图片", "", "*.jpg;;*.png;;All Files(*)")    # 弹出一个文件选择框，第一个返回值imgName记录选中的文件路径+文件名，第二个返回值imgType记录文件的类型
-    Img = QtGui.QPixmap(imgName).scaled(imgView.width()-2, imgView.height()-2)  # 通过文件路径获取图片文件，并设置图片长宽为显示控件大小
+    if imgName == "":
+        return
+    img = QPixmap(imgName).scaled(imgView.width()-2, imgView.height()-2)  # 通过文件路径获取图片文件，并设置图片长宽为显示控件大小
     scene = QGraphicsScene()  # 创建一个图片元素
-    scene.addPixmap(Img)  # 将图片元素添加到图片场景中
+    scene.addPixmap(img)  # 将图片元素添加到图片场景中
     imgView.setScene(scene)  # 将图片场景添加到图片视图中
     imgView.show()  # 显示图片视图
- 
+    self.img[imgG] = cv2.imread(imgName)
+    
+def AlignImage(self, imgView):
+    if 'ref' not in self.img  or 'align' not in self.img :
+        QMessageBox.warning(self, "警告", "未上传{:}".format('基准图片' if 'ref' not in self.img else '待配准图片'))
+    _, self.img['aligned'] = Align(self.img['ref'], self.img['align'])
+    # cv2.imshow('alignedImg', self.img['aligned'])
+    img = cv2.cvtColor(self.img['aligned'], cv2.COLOR_BGR2RGB) # 转换图像通道
+    rows, cols, channels = img.shape # 获取图像形状
+    bytesPerLine = channels * cols # 每行的字节数
+    QImg = QImage(img.data, cols, rows, bytesPerLine, QImage.Format_RGB888) # 依据图像数据构造QImage
+    img = QPixmap.fromImage(QImg).scaled(
+            imgView.width()-2, imgView.height()-2) # 将QImage转换为QPixmap
+    scene = QGraphicsScene()  # 创建一个图片元素
+    scene.addPixmap(img)  # 将图片元素添加到图片场景中
+    imgView.setScene(scene)  # 将图片场景添加到图片视图中
+    imgView.show()  # 显示图片视图
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
     alignDialog = AlignDialog()
+    refImg = None
+    alignImg = None
     myWin.show()
     myWin.buttonOpenAlignDialog.clicked.connect(alignDialog.show)
-    alignDialog.buttonUploadRefImg.clicked.connect(lambda:openImage(alignDialog, alignDialog.refImgView))
-    alignDialog.buttonUploadAlignImg.clicked.connect(lambda:openImage(alignDialog, alignDialog.alignImgView))
+    
     sys.exit(app.exec_())  
